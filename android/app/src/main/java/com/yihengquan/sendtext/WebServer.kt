59c1c8d7bad23934e03a69b41da96edaeeaf958e
Yiheng (NUC) <development.henryquan@gmail.com>
2020-05-15 22:12:09 +1000

    updated web server to be more readable?

--- android/app/src/main/java/com/yihengquan/sendtext/WebServer.kt
+++ android/app/src/main/java/com/yihengquan/sendtext/WebServer.kt
@@ -1,15 +1,23 @@
 package com.yihengquan.sendtext
 
+import android.util.Log
 import fi.iki.elonen.NanoHTTPD
 import java.io.UnsupportedEncodingException
 
-class WebServer(port: Int, private var message: String) : NanoHTTPD(port) {
-    fun setMessage(msg: String) {
-        message = msg
-    }
+/**
+ * A simple webserver class that handles responses, now only supports a text message
+ */
+class WebServer(port: Int) : NanoHTTPD(port) {
+    var message: String
+        get() = message
+        set(value) {
+            // Only update if the new message is meaningful
+            if (!value.isNullOrBlank()) message = value
+        }
 
     override fun serve(session: IHTTPSession): Response {
         var encode = ""
+
         try {
             // Working utf-8 encoding
             val kanji = message.toByteArray(charset("UTF-8"))
@@ -17,8 +25,10 @@ class WebServer(port: Int, private var message: String) : NanoHTTPD(port) {
                 encode += String.format("\\x%2x", b)
             }
         } catch (e: UnsupportedEncodingException) {
+            Log.i("WebServer", "Encoding is not supported")
             e.printStackTrace()
         }
+
         return newFixedLengthResponse(encode)
     }
 
