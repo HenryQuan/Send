f5cc3e40573533b12734c96525bba4c4f6e709e1
Yiheng (NUC) <development.henryquan@gmail.com>
2020-05-15 22:04:09 +1000

    updated deprecated class

--- android/app/src/androidTest/java/com/yihengquan/sendtext/ExampleInstrumentedTest.kt
+++ android/app/src/androidTest/java/com/yihengquan/sendtext/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.yihengquan.sendtext
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,12 +11,12 @@ import org.junit.runner.RunWith
  *
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         Assert.assertEquals("com.yihengquan.sendtext", appContext.packageName)
     }
 }
