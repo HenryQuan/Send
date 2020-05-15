f02c33ec55474a0beedde249311345b46008d029
Yiheng (NUC) <development.henryquan@gmail.com>
2020-05-15 22:38:32 +1000

    updated main activity with more functions and kotlin block
    next step new ui

--- android/app/src/main/java/com/yihengquan/sendtext/MainActivity.kt
+++ android/app/src/main/java/com/yihengquan/sendtext/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.ConnectivityManager
 import android.net.Uri
 import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -20,18 +21,23 @@ import java.net.InetAddress
 import java.net.UnknownHostException
 
 class MainActivity : AppCompatActivity() {
+
     private val port = 9587
-    private val server = WebServer(port, "")
+    private val server = WebServer(port)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
         try {
             // Start server
-            server.start()
+            server.start(2000)
         } catch (e: IOException) {
             e.printStackTrace()
+            Log.i("MainActivity", "Timeout or server is already running")
         }
 
         // Focus inputBox
@@ -42,31 +48,52 @@ class MainActivity : AppCompatActivity() {
 
         // Check if wifi is connected
         val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
-        if (wifi.isConnected) {
+        val wifi = cm.activeNetworkInfo
+        if (wifi != null) {
             // Get device's IP address
             val wm = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-            var ip = intToInetAddress(wm.connectionInfo.ipAddress).toString()
-            ip = ip.replace("/", "") + ':' + port
-            val ipLabel = findViewById<TextView>(R.id.idLabel)
-            ipLabel.text = ip
+            var ip = numberToIPAddress(wm.connectionInfo.ipAddress)
+            if (ip != null) {
+                ip = ip.replace("/", "") + ':' + port
+                val ipLabel = findViewById<TextView>(R.id.idLabel)
+                ipLabel.text = ip
+            } else {
+                // Lock everything
+                showAlertDialog(
+                        "IP address is not valid",
+                        "This usually shouldn't happen.\nPlease try again later.",
+                        cancelable = false,
+                        closeApp = false
+                )
+            }
         } else {
             // Hotspot might work but I don't know
-            AlertDialog.Builder(this)
-                    .setTitle("WiFi is not connected")
-                    .setMessage("Please connect to the same network first")
-                    .setCancelable(false) // Specifying a listener allows you to take an action before dismissing the dialog.
-                    // The dialog is automatically dismissed when a dialog button is clicked.
-                    .setPositiveButton(android.R.string.ok) { dialog, which -> finish() }
-                    .show()
+            showAlertDialog(
+                    "Not connected",
+                    "Please connect to the network",
+                    cancelable = false,
+                    closeApp = true
+            )
         }
     }
 
+    private fun showAlertDialog(title: String, message: String, cancelable: Boolean = true, closeApp: Boolean = false) {
+        AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setCancelable(cancelable) // Specifying a listener allows you to take an action before dismissing the dialog.
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    if (closeApp) finish()
+                    else null // this just closes the dialog
+                }
+                .show()
+    }
+
     fun sendText(v: View?) {
-        val input = findViewById<EditText>(R.id.inputText)
+        val input: EditText = findViewById<EditText>(R.id.inputText)
         val msg = input.text.toString()
-        println(msg)
-        server.setMessage(msg)
+        server.message = msg
     }
 
     /**
@@ -75,15 +102,19 @@ class MainActivity : AppCompatActivity() {
      * @param hostAddress
      * @return
      */
-    private fun intToInetAddress(hostAddress: Int): InetAddress {
+    private fun numberToIPAddress(hostAddress: Int): String? {
         val addressBytes = byteArrayOf((0xff and hostAddress).toByte(),
                 (0xff and (hostAddress shr 8)).toByte(),
                 (0xff and (hostAddress shr 16)).toByte(),
                 (0xff and (hostAddress shr 24)).toByte())
+
+        // Return "" if the host is not valid, this shouldn't happen
         return try {
-            InetAddress.getByAddress(addressBytes)
+            InetAddress.getByAddress(addressBytes).toString()
         } catch (e: UnknownHostException) {
-            throw AssertionError()
+            Log.i("MainActivity", "Host is unknown")
+            e.printStackTrace()
+            null
         }
     }
 
@@ -97,15 +128,15 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-        return if (id == R.id.action_settings) {
-            true
-        } else super.onOptionsItemSelected(item)
+       return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onDestroy() {
-        super.onDestroy()
         server.stop()
+        super.onDestroy()
     }
 
     fun checkLocalhost(item: MenuItem?) {
