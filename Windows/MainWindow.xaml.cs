df0b40ce386d66b7a4b4fcea3955d3866803f471
Yiheng (NUC) <development.henryquan@gmail.com>
2020-05-30 15:21:09 +1000

    start working on ui

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace Send
 {
@@ -16,9 +17,14 @@ namespace Send
         /// </summary>
         private int delay = 1000;
 
+        private string status = "NOT CONNECTED";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Setup UI
+            updateStatusText();
         }
     
         
@@ -29,10 +35,37 @@ namespace Send
             SendKeys.SendWait("^(a)");
         }
 
+        #region IP related
+
+        #endregion
+
+        #region Web request
+
+        #endregion
+
+        #region UI update
+
+        /// <summary>
+        /// It can either be online or offline with green or red colour
+        /// </summary>
+        private void updateStatusText()
+        {
+            statusText.Text = status;
+            if (status.Equals("CONNECTED"))
+            {
+                statusText.Foreground = Brushes.DarkGreen;
+            } else
+            {
+                statusText.Foreground = Brushes.Red;
+            }
+        }
+
         private void delaySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             delay = (int)(e.NewValue * 100);
             delayText.Text = $"Delay: {delay}ms";
         }
+
+        #endregion
     }
 }
