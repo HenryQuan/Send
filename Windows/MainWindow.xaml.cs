a1ce58168ef9ea07dbdb7618b4f54e1d693e937d
Yiheng (NUC) <development.henryquan@gmail.com>
2020-05-30 15:11:19 +1000

    Link delay

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Forms;
 
 namespace Send
@@ -9,16 +11,28 @@ namespace Send
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// The default delay is 1s, 1000ms
+        /// </summary>
+        private int delay = 1000;
+
         public MainWindow()
         {
             InitializeComponent();
-            sendKeys();
         }
+    
+        
 
         private async void sendKeys()
         {
             await Task.Delay(3000);
             SendKeys.SendWait("^(a)");
+        }
+
+        private void delaySlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            delay = (int)(e.NewValue * 100);
+            delayText.Text = $"Delay: {delay}ms";
         }
     }
 }
