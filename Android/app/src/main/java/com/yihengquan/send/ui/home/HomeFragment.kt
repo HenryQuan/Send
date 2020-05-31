93796102a20b3c99e189f988f73be52d6e45536d
Yiheng (NUC) <development.henryquan@gmail.com>
2020-05-31 15:04:07 +1000

    pick image and file
    how to host though

--- Android/app/src/main/java/com/yihengquan/send/ui/home/HomeFragment.kt
+++ Android/app/src/main/java/com/yihengquan/send/ui/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.yihengquan.send.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.yihengquan.send.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -32,11 +35,28 @@ class HomeFragment : Fragment() {
                 }
             }
 
+            sendFileButton.setOnClickListener {
+                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                intent.type = "file/*"
+                startActivityForResult(intent, 0)
+            }
+
+            sendImageButton.setOnClickListener {
+                val intent = Intent(Intent.ACTION_GET_CONTENT)
+                intent.type = "image/*"
+                startActivityForResult(intent, 1)
+            }
+
             homeViewModel.address.observe(viewLifecycleOwner, Observer {
                 myAddressView.text = it
             })
         }
 
         return binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i("HomeFragment", data.toString())
     }
 }
